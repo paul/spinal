b81afed3fead7212f45aec6f9da834821a3b3a97
Paul Sadauskas <psadauskas@gmail.com>
2010-06-27 19:34:52 -0600

    Fix rspec code

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ gem 'activesupport', '>=3.0.0.beta4'
 
 gem 'rack'
 
+group :dev do
+  gem 'jeweler'
+  gem 'yard'
+end
+
 group :test do
   gem 'rspec', '>=2.0.0.beta.13'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'spinal'
-require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
-  
+require 'rspec/core'
+
+RSpec.configure do |config|
+
 end
